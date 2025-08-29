5b1cb7d595afb28023a60fe653343f342de31193
The Six Million Dollar Man <steve@austin.com>
2025-08-29 16:59:20 +0100

    Added make application install

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Debian/Ubuntu
+sudo apt-get update
+sudo apt-get install -y cmake build-essential
+
+# Fedora
+#sudo dnf install cmake make gcc-c++
+
+# Arch
+#sudo pacman -S cmake base-devel
+
+
 # inside project root
 sudo mkdir build && cd build
 cmake ..
