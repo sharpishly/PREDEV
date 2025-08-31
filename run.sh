373600f221c1c4e85c6ffa980eedbbe81aacbc8f
Steve Austin <steve@austin.com>
2025-08-31 15:33:51 +0100

    #1 Changed permissions to address the 404 issue

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo ufw allow 1966
-sudo ufw status
+#sudo ufw allow 1966
+#sudo ufw status
+
+chmod -R u+rwX SharpishlyApp/src/View/www
+
+ls -l SharpishlyApp/src/View/www
 
 cd SharpishlyApp/build
 
