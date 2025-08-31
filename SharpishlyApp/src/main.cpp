be617ac1fdca4b11522f11a35ea996d62ae72c7a
Steve Austin <steve@austin.com>
2025-08-31 15:52:32 +0100

    #1 Added Nginx stop command

--- SharpishlyApp/src/main.cpp
+++ SharpishlyApp/src/main.cpp
@@ -2,7 +2,7 @@
 #include "View/HttpServer.h"
 
 int main() {
-    HttpServer server("127.0.0.1", 1966);
+    HttpServer server("0.0.0.0", 1966);
     server.start();
 
     std::cout << "Press Enter to stop server..." << std::endl;
--- run.sh
+++ run.sh
@@ -4,7 +4,16 @@ sudo ufw allow 1966
 
 sudo ufw status
 
+# Set permissions
 sudo chmod -R u+rwX SharpishlyApp/src/View/www
+
+# Stop Nginx
+sudo systemctl stop nginx
+sudo service nginx stop
+sudo systemctl status nginx
+# or
+sudo service nginx status
+
 
 ls -l SharpishlyApp/src/View/www
 
