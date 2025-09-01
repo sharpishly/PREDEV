945b704c76995e72aa034493899d80a880473e4a
Steve Austin <steve@austin.com>
2025-09-01 14:52:12 +0100

    #1 Added comments and echo message for build file

--- run.sh
+++ run.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
 
+# ------------------------------
+# Allow port 1966
+# -----------------------------
+echo ">>> Allow port 1966..."
 sudo ufw allow 1966
 
+# ------------------------------
+# Port status
+# -----------------------------
+echo ">>> Port status..."
 sudo ufw status
 
+# ------------------------------
 # Set permissions
+# -----------------------------
+echo ">>> Set permissions..."
 sudo chmod -R u+rwX SharpishlyApp/src/View/www
 
+# ------------------------------
 # Stop Nginx
+# -----------------------------
+echo ">>> Stop Nginx..."
 sudo systemctl stop nginx
 sudo service nginx stop
-# sudo systemctl status nginx
-# or
-# sudo service nginx status
 
-
+# ------------------------------
+# List www directory
+# -----------------------------
+echo ">>> List www directory..."
 ls -l SharpishlyApp/src/View/www
 
+# ------------------------------
+# Change to build directory
+# -----------------------------
+echo ">>> Change to build directory..."
 cd SharpishlyApp/build
 
+# ------------------------------
+# Compile and build
+# -----------------------------
+echo ">>> Compile and build..."
 cmake ..
-
 make
 
+# ------------------------------
+# Run application
+# -----------------------------
+echo ">>> Run application..."
 ./SharpishlyApp
