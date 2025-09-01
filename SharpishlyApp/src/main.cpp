5b277c6e8a9f2df14fb7e923b8928b71dea13bc8
Steve Austin <steve@austin.com>
2025-09-01 16:34:56 +0100

    #3 Added Updated main to include routing functionality

--- SharpishlyApp/src/main.cpp
+++ SharpishlyApp/src/main.cpp
@@ -1,23 +1,27 @@
+#include "HttpServer.h"
 #include "Router.h"
 #include "Controller/HomeController.h"
 
 int main() {
     Router router;
 
-    // Static route
+    // Example routes
     router.addRoute("home/index", [](const std::vector<std::string>& params) {
         HomeController hc;
         return hc.index(params);
     });
 
-    // Dynamic route
     router.addRoute("user/{id}", [](const std::vector<std::string>& params) {
-        return "<html><body><h1>User Profile</h1><p>User ID: " + params[0] + "</p></body></html>";
+        return "<h1>User Profile</h1><p>User ID: " + params[0] + "</p>";
     });
 
     router.addRoute("post/{category}/{slug}", [](const std::vector<std::string>& params) {
-        return "<html><body><h1>Post</h1><p>Category: " + params[0] + "<br>Slug: " + params[1] + "</p></body></html>";
+        return "<h1>Post</h1><p>Category: " + params[0] + "<br>Slug: " + params[1] + "</p>";
     });
 
-    // TODO: Pass router to HttpServer
+    HttpServer server("127.0.0.1", 1966, router);
+    server.start();
+
+    std::cin.get(); // Keep running until Enter is pressed
+    server.stop();
 }
